a0a41d3ca215c196447847602ba0ef43347e5964
softscrape <softscrape@gmail.com>
2018-07-20 13:19:40 +0300

    fix debug API for some compiller versions

--- common/dsmisc.h
+++ common/dsmisc.h
@@ -13,9 +13,9 @@ void dstrace(const char *msg, ...);
 void dstracerr(int err, const char *msg, ...);
 void dsdump(const unsigned char *buf, int buf_size);
 #else
-inline void dstrace(const char *msg, ...) {};
-inline void dstracerr(int err, const char *msg, ...) {};
-inline void dsdump(const unsigned char *buf, int buf_size) {};
+#define dstrace(...)
+#define dstracerr(...)
+#define dsdump(...)
 #endif
 
 #endif /* __DSMISC_H__ */
